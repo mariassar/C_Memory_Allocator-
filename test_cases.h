e4d21f067fe0e78228231538c35101122c3e1ab2
Mariassa Royzenberg <93010218+mariassar@users.noreply.github.com>
2023-12-12 17:29:44 -0500

    Update test_cases.h

--- test_cases.h
+++ test_cases.h
@@ -3,8 +3,8 @@
 
 /* decleration of test funtions */
 void test_mem_init(unsigned char *global_memory, unsigned int global_mem_size);
-void test_allocate_more_than_available_memory(global_memory, global_mem_size));
-void test_free_null_pointer();
-void test_allocate_free_random_sizes();
+void test_allocate_more_than_available_memory(unsigned char *global_memory, unsigned int global_mem_size);
+void test_free_null_pointer(unsigned char *global_memory, unsigned int global_mem_size);
+void test_allocate_free_random_sizes(unsigned char *global_memory, unsigned int global_mem_size);
 
 #endif
